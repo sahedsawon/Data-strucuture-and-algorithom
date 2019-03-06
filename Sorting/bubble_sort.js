0bacced55b65734de1e4e479f26c2ae8307e24e4
sahedsawon <smsawoncse@gmail.com>
2019-03-06 17:54:10 +0600

    selection sort bubble sort implemented

--- Sorting/bubble_sort.js
+++ Sorting/bubble_sort.js
@@ -3,9 +3,7 @@ function buble_sort() {
 
   for (var i = 0; i < arr.length - 1; i++) {
     for (var j = 0; j < arr.length - i - 1; j++) {
-      console.log(arr[j] + ' ' + arr[j + 1]);
-
-      if (arr[j] > arr[j + 1]) {
+        if (arr[j] > arr[j + 1]) {
         var temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
